8b59af60837fe565849da5796c03a6daf0f0c08a
Scott Travis <scott@swt83.com>
2012-03-28 10:19:57 -0400

    Added start.php

---
+++ libraries/wp.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * A LaravelPHP package for working w/ the Wordpress API.
+ *
+ * @package    Wordpress
+ * @version    1.0
+ * @author     Scott Travis <scott.w.travis@gmail.com>
+ * @link       http://github.com/swt83/laravel-wp
+ * @license    MIT License
+ */
+
+class WP
+{
+	/**
+	 * Magic method to execute all methods for JSON API.
+	 */
+	public static function __callStatic($method, $args)
+	{
+		// load config
+		extract(Config::get('wordpress::wp'));
+	
+		// build url
+		$url .= '?json='.$method;
+		if (!empty($args))
+		{
+			foreach($args[0] as $key=>$value)
+			{
+				$url .= '&'.$key.'='.urlencode($value);
+			}
+		}
+		
+		// add multisite option
+		if ($site_id !== null) $url .= '&site_id='.urlencode($site_id);
+		
+		// connect to api
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		$response = curl_exec($ch);
+		curl_close($ch);
+		
+		// return response
+		$result = @json_decode($response);
+		if ($result !== false and $result !== null)
+		{
+			return $result;
+		}
+		else
+		{
+			return false;
+		}
+	}
+}
---
+++ start.php
@@ -0,0 +1,5 @@
+<?php
+
+Autoloader::map(array(
+	'WP' => __DIR__.'/libraries/wp.php',
+));
